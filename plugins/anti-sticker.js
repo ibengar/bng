92805d29439124e9deaf3c83bcad8a9cbcb781b3
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-05 07:23:47 +0800

    Update anti-sticker.js

--- plugins/anti-sticker.js
+++ plugins/anti-sticker.js
@@ -13,9 +13,9 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
       if (global.opts) {
         if (isAdmin || !isBotAdmin){		  
         } else {
-          m.reply('*Sticker detected*') // ganti text terserah kamu 
+          m.reply('*Sticker lu meresahkan*') // ganti text terserah kamu 
           global.db.data.users[m.sender].warn += 1
-    global.db.data.users[m.sender].banned = true
+    global.db.data.users[m.sender].banned = false
     return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
         }return true
       }
