f112bbbeca90bbd17169f074a5fed5d16e4f45f0
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-05 16:44:35 +0800

    Update all-antilink.js

--- plugins/all-antilink.js
+++ plugins/all-antilink.js
@@ -3,84 +3,45 @@ const isLinkYt = /youtube.com|youtu.be/i // tambahin sendiri
 const isLinkTel = /t.me/i // tambahin sendiri
 const isLinkFb = /facebook.com|fb.me/i // tambahin sendiri
 const isLinkIg = /instagram.com/i // tambahin sendiri
-// const isLinkWa = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i // tambahin sendiri
 const isLinkHttp = /http|https/i // tambahin sendiri
 
 export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isAntiLinkTik = isLinkTik.exec(m.text)
-    const isAntiLinkYt = isLinkYt.exec(m.text)
-   // const isAntiLinkWa = isLinkWa.exec(m.text)
-    const isAntiLinkTel = isLinkTel.exec(m.text)
-    const isAntiLinkFb = isLinkFb.exec(m.text)
-    const isAntiLinkIg = isLinkIg.exec(m.text)
-    const isAntiLinkHttp = isLinkHttp.exec(m.text)
-    let hapus = m.key.participant
-    let bang = m.key.id
-    if (chat.antiLinkTik && isAntiLinkTik) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antilinktik', '/disable antilinktik'], m)
-        if (isBotAdmin && bot.restrict) {
-  //  user.warn += 1
-  //  user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    
-    if (chat.antiLinkYt && isAntiLinkYt) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antilinkyt', '/disable antilinkyt'], m)
-        if (isBotAdmin && bot.restrict) {
-   // user.warn += 1
-   // user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    
-    if (chat.antiLinkTel && isAntiLinkTel) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antilinktel', '/disable antilinktel'], m)
-        if (isBotAdmin && bot.restrict) {
-  //  user.warn += 1
- //   user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    
-    if (chat.antiLinkFb && isAntiLinkFb) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antilinkfb', '/disable antilinkfb'], m)
-        if (isBotAdmin && bot.restrict) {
-  //  user.warn += 1
-  //  user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    
-    if (chat.antiLinkIg && isAntiLinkIg) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antilinkig', '/disable antilinkig'], m)
-        if (isBotAdmin && bot.restrict) {
-  //  user.warn += 1
-   // user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    
-
-    if (chat.antiLinkHttp && isAntiLinkHttp) {
-        await conn.sendButton(m.chat, `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antihatetepe', '/disable antihatetepe'], m)
-        if (isBotAdmin && bot.restrict) {
-    // user.warn += 1
-   // user.banned = false
-    return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*!')
-    }
-    return !0
+	if (m.isBaileys && m.fromMe) return !0
+	if (!m.isGroup) return !1
+	let chat = global.db.data.chats[m.chat]
+	let user = global.db.data.users[m.sender]
+	let bot = global.db.data.settings[this.user.jid] || {}
+	const isAntiLinkTik = isLinkTik.exec(m.text)
+	const isAntiLinkYt = isLinkYt.exec(m.text)
+	const isAntiLinkTel = isLinkTel.exec(m.text)
+	const isAntiLinkFb = isLinkFb.exec(m.text)
+	const isAntiLinkIg = isLinkIg.exec(m.text)
+	const isAntiLinkHttp = isLinkHttp.exec(m.text)
+	let hapus = m.key.participant
+	let bang = m.key.id
+	let bwah = `*Link Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`
+	if (!isAdmin) {
+		if (chat.antiLinkTik && isAntiLinkTik) {
+			await conn.sendButton(m.chat, bwah, author, ['off antilinktik', '/disable antilinktik'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		} else if (chat.antiLinkYt && isAntiLinkYt) {
+			await conn.sendButton(m.chat, bwah, author, ['off antilinkyt', '/disable antilinkyt'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		} else if (chat.antiLinkTel && isAntiLinkTel) {
+			await conn.sendButton(m.chat, bwah, author, ['off antilinktel', '/disable antilinktel'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		} else if (chat.antiLinkFb && isAntiLinkFb) {
+			await conn.sendButton(m.chat, bwah, author, ['off antilinkfb', '/disable antilinkfb'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		} else if (chat.antiLinkIg && isAntiLinkIg) {
+			await conn.sendButton(m.chat, bwah, author, ['off antilinkig', '/disable antilinkig'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		} else if (chat.antiLinkHttp && isAntiLinkHttp) {
+		await conn.sendButton(m.chat, bwah, author, ['off antihatetepe', '/disable antihatetepe'], m)
+			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+		}
+	} else {
+		await m.reply('*Anda Atmin Anda Aman :v!*!')
+	}
+	return !0
 }
