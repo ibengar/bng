230449ebc9441a1585af2db7d7f0ec5758ebf6f0
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-07 06:26:34 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -7,21 +7,12 @@ try {
 let p = await tiktok(args[0])
     if (!p.link) throw 'Can\'t download video!'
     let cap = `*「 T I K T O K 」*
-                 ████████▀▀▀████
-                 ████████────▀██
-                 ████████──█▄──█
-                 ███▀▀▀██──█████
-                 █▀──▄▄██──█████
-                 █──█████──█████
-                 █▄──▀▀▀──▄█████
-                 ███▄▄▄▄▄███████
-──────────────────────────
-
+                 
 *📛Nickname:* ${p.dev}
 *📒Description:* ${p.description}
 *Url:* ${p.url}
 `.trim()
-conn.send2ButtonVid(m.chat, p.link, cap, author, `Audio`, `.tiktokaudio ${args[0]}`, fakes, adReply)
+conn.send2ButtonVid(m.chat, p.link, cap, author, `No Wm`, `.tiktok ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, fakes, adReply)
 } catch (e) {
     throw eror
     }
