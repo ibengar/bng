64c7622314baa52e2015cb8b30744a6b8a22d7fe
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-08 09:52:15 +0800

    Update all-antilink.js

--- plugins/all-antilink.js
+++ plugins/all-antilink.js
@@ -2,7 +2,7 @@ const isLinkTik = /tiktok.com/i // tambahin sendiri
 const isLinkYt = /youtube.com|youtu.be/i // tambahin sendiri
 const isLinkTel = /t.me/i // tambahin sendiri
 const isLinkFb = /facebook.com|fb.me/i // tambahin sendiri
-const isLinkIg = /instagram.com/i // tambahin sendiri
+const isLinkBatu = /🗿/i // tambahin sendiri
 const isLinkHttp = /http|https/i // tambahin sendiri
 
 export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
@@ -15,7 +15,7 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
 	const isAntiLinkYt = isLinkYt.exec(m.text)
 	const isAntiLinkTel = isLinkTel.exec(m.text)
 	const isAntiLinkFb = isLinkFb.exec(m.text)
-	const isAntiLinkIg = isLinkIg.exec(m.text)
+	const isAntiLinkIg = isLinkBatu.exec(m.text)
 	const isAntiLinkHttp = isLinkHttp.exec(m.text)
 	let hapus = m.key.participant
 	let bang = m.key.id
@@ -33,8 +33,8 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
 		} else if (chat.antiLinkFb && isAntiLinkFb) {
 			await conn.sendButton(m.chat, bwah, author, ['off antilinkfb', '/disable antilinkfb'], m)
 			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
-		} else if (chat.antiLinkIg && isAntiLinkIg) {
-			await conn.sendButton(m.chat, bwah, author, ['off antilinkig', '/disable antilinkig'], m)
+		} else if (chat.antiLinkBatu && isAntiLinkBatu) {
+			await conn.sendButton(m.chat, bwah, author, ['off antibatu🗿', '/disable antibatu🗿'], m)
 			if (isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
 		} else if (chat.antiLinkHttp && isAntiLinkHttp) {
 		await conn.sendButton(m.chat, bwah, author, ['off antihatetepe', '/disable antihatetepe'], m)
