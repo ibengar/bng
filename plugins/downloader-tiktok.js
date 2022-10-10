79fda3686eba5e71760f3aae390494405ecde4a5
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-10 15:43:04 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,21 +1,39 @@
-import fetch from 'node-fetch'
-
+let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-m.reply(wait)
-let res = await fetch(`https://ibeng-api.ddns.net/api/downloader/tiktok2?url=${args[0]}&apikey=ibeng
-if (!res.ok) throw await res.text()
-let json = await res.data.json()
-if (!json.status) throw json
-let video { result.video } = json.result
-await conn.sendFile(m.chat, video, 'video.mp4', `
-`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('ibeng', '/api/downloader/tiktok2', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  /*let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = json.info[0].text
+  for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+▶ ${json.info[0].playCount} Views
+❤ ${json.info[0].diggCount} Likes
+🔁 ${json.info[0].shareCount} Shares
+💬 ${json.info[0].commentCount} Comments
+🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
+- *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
+- *Desc:*
+${txt}
+  `.trim(), m)*/
+  let url = json.result.link_dl1 || json.result.link_dl2 || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = `
+  - *By:* ${json.result.name}
+  - *Caption:*
+  ${json.result.caption}
+    `
+    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
-
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(tiktok)$/i
+handler.command = /^(tik(tok)?(dl)?)$/i
 handler.limit = true
 
-export default handler
+module.exports = handler
