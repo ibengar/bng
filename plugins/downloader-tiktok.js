24169a8fa0b18381d6fc34a9b5baee4edb60c6d9
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-16 19:34:38 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,21 +1,34 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
+import { tiktok } from "social_media_downloader"
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+if (!args[0]) throw 'Masukkan Link'
+try {
+let p = await tiktok(args[0])
+    if (!p.link) throw 'Can\'t download video!'
+    let cap = `*「 T I K T O K 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+──────────────────────────
 
-let handler = async (m, { conn, args }) => {
-    if (!args[0]) throw 'Uhm..url nya mana?'
-    m.reply(wait)
-    let res = await fetch(`https://ibeng-api.ddns.net/api/downloader/tiktok2?url=${args[0]}&apikey=ibeng`)
-    if (!res.ok) throw await res.text()
-    let json = await res.data.json()
-    if (!json.status) throw json
-    let { video } = json.result
-    await conn.sendFile(m.chat, video, 'video.mp4', `
-    `, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+*📛Nickname:* ${p.dev}
+*📒Description:* ${p.description}
+*Url:* ${p.url}
+`.trim()
+conn.send2ButtonVid(m.chat, p.link, cap, author, `No Wm`, `.tiktok ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, fakes, adReply)
+} catch (e) {
+    throw eror
+    }
 }
-
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(tiktok)$/i
-handler.limit = true
+handler.command = /^t(iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
 
 export default handler
