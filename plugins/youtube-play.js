0f23f1e730afb5650e9ca46484790c38ac266db2
ibengar <56824876+ibengar@users.noreply.github.com>
2022-10-21 10:52:23 +0700

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,45 +1,34 @@
+// Nyoba downloader hasil recode sendiri ngbab
+// Visit web nya (link): https://botcahx2.ddns.net
+// Penggunaan filter audio dan video
+// Example:
+// filter=videoonly&quality=highestvideo&contenttype=video/mp4
+// filter=audioonly&quality=highestaudio&contenttype=audio/mpeg
+// Akhir hasil :
+// https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg
+
+
+
 import fetch from 'node-fetch'
 import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
-try {
-  if (!text) throw `Use example ${usedPrefix}${command} gustixa`
-  let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-  const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.senButtonImg(m.chat, `*YOUTUBE DOWNLOADER*
-📌 *Title:* ${title}
-🔗 *Url:* ${url}
-🖹 *Description:* ${description}
-⏲️ *Published:* ${publishedTime}
-⌚ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
-  `.trim(), author, thumbnail, url, '', null, null, [
-    ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
-    ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
-    
-  ], m)
-} catch {
-if (!text) throw 'Input Query'
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) throw 'Input Query'
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Tidak Ditemukan'
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
-  let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
+  let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Description:* ${description}\n*Url:* ${url}`
   let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
   let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
-  // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
+  //if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
 }
-
-}
-handler.help = ['play', 'play'].map(v => v + ' <pencarian>')
+handler.help = ['play']
 handler.tags = ['downloader']
-handler.command = /^(y((outube|((utu|t)b|t))play|tp)|play(yt)?)$/i
-
+handler.command = /^(play)$/i
 handler.exp = 0
-handler.limit = false
 
 export default handler
 
