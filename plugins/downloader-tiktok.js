eeea3ea9f63c792922a0aa0eaffc89ff57d77438
ibengar <56824876+ibengar@users.noreply.github.com>
2022-11-04 17:37:38 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply('Tunggu sebentar...')
-let res = await fetch(`https://botcahx.ddns.net/api/dowloader/tikok?url=${args[0]}`)
+let res = await fetch(`https://fatiharridho.herokuapp.com/api/downloader/tiktok/nowm?url=${args[0]}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
